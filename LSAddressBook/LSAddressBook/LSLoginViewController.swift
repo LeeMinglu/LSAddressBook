b2e3ff22afac419951b5bf26674a81bd18031a39
LeeMinglu <402007353@qq.com>
2017-01-14 20:48:41 +0800

    实现可自动登陆

---
+++ LSAddressBook/LSAddressBook/LSLoginViewController.swift
@@ -0,0 +1,139 @@
+//
+//  LSLoginViewController.swift
+//  LSAddressBook
+//
+//  Created by 李明禄 on 2017/1/6.
+//  Copyright © 2017年 SocererGroup. All rights reserved.
+//
+
+import UIKit
+
+
+class LSLoginViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var remberPwd: UISwitch!
+    @IBOutlet weak var autoLogin: UISwitch!
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.accountTextField.text! = "rock"
+        self.passwordTextField.text! = "rock"
+        
+//        self.passwordTextField.delegate = self
+//        self.accountTextField.delegate = self
+        
+        self.passwordTextField.addTarget(self, action: #selector(textChange), for: UIControlEvents.editingChanged)
+//
+        self.accountTextField.addTarget(self, action: #selector(textChange), for: UIControlEvents.editingChanged)
+        
+//
+        self.textChange()
+        
+        if self.autoLogin.isOn {
+            self.clickLoginBtn(Any.self)
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textChange() {
+
+        if !(self.accountTextField.text!.isEmpty) &&
+            !(self.passwordTextField.text!.isEmpty) {
+              self.loginBtn.isEnabled = true
+        } else {
+            self.loginBtn.isEnabled = false
+        }
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func autoLogin(_ sender: Any) {
+        print("切换了自动登陆")
+       
+        
+    }
+    
+    @IBAction func remberPwd(_ sender: Any) {
+        print("切换了记住密码")
+    }
+    
+    @IBAction func clickLoginBtn(_ sender: Any) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        
+        if self.accountTextField.text! == "rock" && self.passwordTextField.text! == "rock" {
+           
+            
+
+//            SVProgressHUD.showInfo(withStatus: "正在登陆")
+//            MBProgressHUD.showMessage("wgb")
+//            SVProgressHUD.showProgress(3.3)
+            SVProgressHUD.show(withStatus: "正在登陆")
+   
+            
+            if !(self.remberPwd.isOn)   {
+                self.accountTextField.text = ""
+                self.passwordTextField.text = ""
+            } else {
+                print("此时会记住密码")
+            }
+            
+            let three = DispatchTime.now() + 2.0
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.0, execute: {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            })
+            DispatchQueue.main.asyncAfter(deadline: three, execute: {
+                self.performSegue(withIdentifier: "login2Contact", sender: nil)
+            })
+            
+
+
+        }else {
+            print("用户名或密码不对")
+           
+            
+           
+            
+             SVProgressHUD.showError(withStatus: "用户名或密码错误")
+        }
+        
+        
+        
+    }
+    
+    
+     // MARK: - textFieldDelegate方法
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        
+//        print(textField.text!)
+//        return true
+//    }
+//    
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        print(textField.text!)
+//    }
+//    
+//    
+    
+
+}
